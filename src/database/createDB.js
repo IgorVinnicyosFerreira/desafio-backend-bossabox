750f3157805813da4cf9aa01ca4adeb21e24522d
unknown <vinicyosferreira@gmail.com>
2020-01-23 10:32:28 -0300

    Configurações sequelize banco de testes e banco postgresql

---
+++ src/database/createDB.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const { Client } = require('pg');
+
+const client = new Client({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT
+});
+
+client.connect();
+
+client.query(`CREATE DATABASE ${process.env.DB_NAME}`, error => {
+  client.end();
+  if (error) console.log(error);
+  process.exit(0);
+});
